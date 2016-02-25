744d8410a8c6fcaae8bcebb11d2a8b06fc8a1b1c
Hai Nguyen <hainn.dev@gmail.com>
2016-02-25 22:58:40 +0700

    new project

---
+++ StaticTableDemo/StaticTableDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StaticTableDemo
+//
+//  Created by Hai Nguyen on 2/25/16.
+//  Copyright © 2016 Hai Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StaticTableDemo/StaticTableDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticTableDemo
+//
+//  Created by Hai Nguyen on 2/25/16.
+//  Copyright © 2016 Hai Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
